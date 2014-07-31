8c4d7afca01e8401ce7913b8bb836a3e75b2a123
Pawel Klimczyk <pawel@klimczyk.pl>
2014-07-31 09:33:38 +0200

    Added  NBPApi sample app project

---
+++ Yaba.NBP.Example/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using NetDevelopersPoland.Yaba.NBP;
+
+namespace Yaba.NBP.Example
+{
+    public partial class MainWindow : Window
+    {
+        private NBPApi nbpApi;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            nbpApi = new NBPApi();
+        }
+
+        private void Button_GetActualExchangeRates_Click(object sender, RoutedEventArgs e)
+        {
+            var list = new List<ExchangeRate>()
+                           {
+                             nbpApi.GetActualExchangeRate(Currency.EUR),
+                             nbpApi.GetActualExchangeRate(Currency.USD),
+                             nbpApi.GetActualExchangeRate(Currency.NOK),
+                             nbpApi.GetActualExchangeRate(Currency.JPY),
+                             nbpApi.GetActualExchangeRate(Currency.GBP)
+                           };
+
+            grid.ItemsSource = list;
+        }
+
+        private void Button_GetActualBuySellRates_Click(object sender, RoutedEventArgs e)
+        {
+            var list = new List<BuySellRate>()
+                           {
+                             nbpApi.GetActualBuySellRate(Currency.EUR),
+                             nbpApi.GetActualBuySellRate(Currency.USD),
+                             nbpApi.GetActualBuySellRate(Currency.NOK),
+                             nbpApi.GetActualBuySellRate(Currency.JPY),
+                             nbpApi.GetActualBuySellRate(Currency.GBP)
+                           };
+
+            grid.ItemsSource = list;
+        }
+
+        private void Button_GetActualBaseRates_Click(object sender, RoutedEventArgs e)
+        {
+            var list = new List<BaseRate>()
+                           {
+                             nbpApi.GetActualBaseRate(Rate.Deposit),
+                             nbpApi.GetActualBaseRate(Rate.Lombard),
+                             nbpApi.GetActualBaseRate(Rate.Reference),
+                             nbpApi.GetActualBaseRate(Rate.Rediscount)
+                           };
+
+            grid.ItemsSource = list;
+        }
+    }
+}
